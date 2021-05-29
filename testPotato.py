beae98c52881226f648fe2302033d1440fa33abb
neromike2 <84219146+neromike2@users.noreply.github.com>
2021-05-29 14:04:09 -0400

    import pygame

--- testPotato.py
+++ testPotato.py
@@ -1,29 +1,33 @@
 import sys, pygame
+
+potatosize=50
 potato = pygame.image.load("potato.png")
+potato = pygame.transform.scale(potato, (potatosize, potatosize))
 pygame.init()
+ship = pygame.image.load("ship2.png")
+ship= pygame.transform.rotate(ship,180)
 speed=20
 agility=5
 size = width, height = 800, 750
-charpos=[width/2,height-50,50,40]
+charpos=[width/2,height-50]
 screen = pygame.display.set_mode(size)
 GameClock = pygame.time.Clock()
 backgroundcolor= 0,0,0
-rectcolor= 200,0,0
-#potato= pygame.image.load("potato.png")
-
 #class bullets:
- #   def __init__(x,y,self,img):
+#    def __init__(x,y,self,img):
 
 
 
-def collide(obj1,obj2):
-    offsetx=obj2.x-obj1.x
-    offsety=obj2.y-obj2.y
-    return obj1.mask.overlap(obj2.mask, (offsetx,offsety))
+#def collide(obj1,obj2):
+#    offsetx=obj2.x-obj1.x
+#    offsety=obj2.y-obj2.y
+#    return obj1.mask.overlap(obj2.mask, (offsetx,offsety))
 
 def reset():
     screen.fill(backgroundcolor)
-    pygame.draw.rect(screen, rectcolor,charpos,0)
+    screen.blit(potato,(100,100))
+    screen.blit(ship,charpos)
+    pygame.display.flip()
 while 1:
     GameClock.tick(speed)
     for event in pygame.event.get():
@@ -32,10 +36,11 @@ while 1:
     keypressed = pygame.key.get_pressed()
     if keypressed[pygame.K_LEFT] and charpos[0]>0:
         charpos[0] = charpos[0]-agility
-    if keypressed[pygame.K_RIGHT] and charpos[0]<(width-charpos[2]):
+    if keypressed[pygame.K_RIGHT] and charpos[0]<(width-40):
         charpos[0] = charpos[0]+agility
+
+
     reset()
    # if keypressed[pygame.K_SPACE]
-
     pygame.display.flip()
 pygame.quit()
