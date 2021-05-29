23fc4b65e11168adc5bcc68094bd6c23ef6327f1
neromike2 <84219146+neromike2@users.noreply.github.com>
2021-05-29 12:09:28 -0400

    import pygame

--- main.py
+++ main.py
@@ -1,2 +1,2 @@
-import pygame
+import sys, pygame
 import testPotato
--- testPotato.py
+++ testPotato.py
@@ -4,7 +4,8 @@ speed=20
 agility=2
 size = width, height = 800, 750
 charpos=[width/2,height-50]
-
+potato = pygame.image.load('potato.png')
+potatorect=potato.get_rect()
 screen = pygame.display.set_mode(size)
 GameClock = pygame.time.Clock()
 backgroundcolor= 0,0,0
@@ -17,15 +18,13 @@ while 1:
     GameClock.tick(speed)
     for event in pygame.event.get():
         if event.type == pygame.QUIT: sys.exit()
-    reset()
-   # screen.blit(char, charpos)
-    pygame.display.flip()
     keypressed= pygame.key.get_pressed()
-    if keypressed==K_LEFT:
-        charpos(0)=charpos(0)-agility
+ #   if keypressed==K_LEFT:
+#        charpos(0)=charpos(0)-agility
 
-
-
-
-
+    reset()
+    screen.bl
+    screen.blit(potato,)
+    # screen.blit(char, charpos)
+    pygame.display.flip()
 pygame.quit()
